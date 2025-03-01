dab5451f6084c1d5735d4a025a518134ece88688
CharaD7 <jijakahn6@gmail.com>
2025-03-01 16:27:01 +0000

    Registered page routes in App.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,30 @@
+<script setup>
+  const menu = [
+    {
+      title: 'Home',
+      path: '/'
+    },
+    {
+      title: 'About',
+      path: '/about_page'
+    },
+    {
+      title: 'Contact',
+      path: '/contact_page'
+    },
+    {
+      title: 'Shop',
+      path: '/shop'
+    }
+  ];
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h2>Layout and Pages</h2>
+  <ul v-if="menu">
+    <li v-for="(item, index) in menu" :key="index">
+      <NuxtLink :to="item.path">{{ item.title }}</NuxtLink>
+    </li>
+  </ul>
+  <NuxtPage />
 </template>
